1699c85b0f00ede7ee603cff18fed9abbeb6b923
Lucas340 <lucasstorres100@gmail.com>
2020-09-30 10:37:15 -0300

    Init api with express and socket.io

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,23 @@
-import express from 'express';
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import socket from 'socket.io'
 
 const app = express()
 
+const io = socket(8080)
+
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+}))
+
+app.use(cors())
+app.use(express.json())
 
 app.listen(3333)
