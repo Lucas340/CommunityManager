b6dceb8d3268281fedc7cda421798e7eee6854bb
Lucas340 <lucasstorres100@gmail.com>
2020-09-30 08:15:43 -0300

    Init Commands

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ const client = new CommunityManager();
 
 // INIT's
 client.initWinston()
+client.initComamnds()
+
 client.login(String(process.env.TOKEN))
   .then(() => client.logger.info('CommunityManager bot is running'))
   .catch(() => client.logger.error('CommunityManager does not online, because an error occurred'))
