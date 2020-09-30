c64c21e6f75b96a6cb0562c292f3fbb7c37fd8b0
Lucas340 <lucasstorres100@gmail.com>
2020-09-30 08:10:33 -0300

    Change support of TypeScript

--- src/CommunityManager.ts
+++ src/CommunityManager.ts
@@ -2,15 +2,20 @@ import winston, { Logger } from 'winston'
 import moment from 'moment'
 import { Client } from 'discord.js'
 
+import glob from 'glob'
+import path from 'path'
+
 moment.locale('pt-br')
 
 class CommunityManager extends Client {
   public logger: Logger;
+  public commands: any[]
   
   constructor () {
     super({})
 
     this.logger = winston.createLogger()
+    this.commands = []
   }
 
   async login(token: string) {
@@ -34,6 +39,13 @@ class CommunityManager extends Client {
     }
   }
 
+  initComamnds() {
+    glob.sync('./src/commands/**/*.js').forEach(file => {
+      this.logger.debug(`Loading command ${file}`)
+      const Command = require(path.resolve(file))
+      this.commands.push(new Command())
+    })
+  }
 }
 
 export default CommunityManager;
