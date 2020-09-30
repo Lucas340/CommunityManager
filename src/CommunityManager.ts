eb58e7f995b2d782a20ec6bb1660524d724f1c5e
Lucas340 <lucasstorres100@gmail.com>
2020-09-29 21:37:01 -0300

    Init the bot connection

--- src/CommunityManager.ts
+++ src/CommunityManager.ts
@@ -2,21 +2,19 @@ import winston, { Logger } from 'winston'
 import { Client } from 'discord.js'
 
 class CommunityManager extends Client {
-  public logger: Logger|null;
+  public logger: Logger;
   
   constructor () {
-    super()
+    super({})
 
-    this.logger = null;
+    this.logger = winston.createLogger()
   }
 
   async login(token: string) {
     return super.login(token)
   }
 
-  async log() { 
-    this.logger = winston.createLogger()
-
+  async initWinston() { 
     if (process.env.NODE_ENV === 'production') {
       this.logger.add(new winston.transports.Console({ level: process.env.LOGGING_LEVEL || 'silly' }))
     } else {
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@ import CommunityManager from "./CommunityManager";
 
 const client = new CommunityManager();
 
+client.initWinston()
 client.login(String(process.env.TOKEN))
+
+client.logger.info('CommunityManager online', { label: "CommunityManager" })
